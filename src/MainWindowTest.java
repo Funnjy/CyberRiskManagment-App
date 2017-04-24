f1c6bb1f529e80c06c3835dc54255900737b8ac9
Anton <ilchenko.anton@gmail.com>
2017-04-24 23:22:49 +0200

    Modification of window width and height

--- src/MainWindowTest.java
+++ src/MainWindowTest.java
@@ -8,12 +8,23 @@ public class MainWindowTest {
     public static void main(String[] args){
 
         EventQueue.invokeLater(new Runnable() {
+
             @Override
             public void run() {
+
                 MainWindow mainW = new MainWindow();
+
                 mainW.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                mainW.setSize(400, 300);
+
+                Toolkit verktøykasse = Toolkit.getDefaultToolkit();
+                Dimension skjermdimensjon = verktøykasse.getScreenSize();
+                int bredde = skjermdimensjon.width;
+                int høyde = skjermdimensjon.height;
+
+                mainW.setSize(bredde/4, høyde/4);
                 mainW.setVisible(true);
+                mainW.setResizable(false);
+                mainW.setLocationRelativeTo(null);
             }
         });
     }
